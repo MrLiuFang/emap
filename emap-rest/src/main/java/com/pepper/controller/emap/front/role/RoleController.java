1e01126b7a69b42775fd237c24517beeb8791043
Administrator <Administrator@192.168.1.106>
2019-09-10 22:22:53 +0800

    1

--- emap-rest/src/main/java/com/pepper/controller/emap/front/role/RoleController.java
+++ emap-rest/src/main/java/com/pepper/controller/emap/front/role/RoleController.java
@@ -1,42 +1,74 @@
 package com.pepper.controller.emap.front.role;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.pepper.controller.emap.core.ResultData;
 import com.pepper.controller.emap.util.Internationalization;
 import com.pepper.core.Pager;
+import com.pepper.core.base.BaseController;
+import com.pepper.core.base.impl.BaseControllerImpl;
 import com.pepper.core.constant.SearchConstant;
+import com.pepper.model.console.admin.user.AdminUser;
 import com.pepper.model.console.role.Role;
+import com.pepper.model.console.role.RoleMenu;
+import com.pepper.model.console.role.RoleUser;
 import com.pepper.service.authentication.aop.Authorize;
+import com.pepper.service.console.admin.user.AdminUserService;
+import com.pepper.service.console.role.RoleMenuService;
 import com.pepper.service.console.role.RoleService;
+import com.pepper.service.console.role.RoleUserService;
+import com.pepper.service.emap.log.SystemLogService;
 import com.pepper.util.MapToBeanUtil;
 
 @Controller("frontRoleController")
 @RequestMapping(value = "/front/role")
 @Validated
-public class RoleController {
+public class RoleController  extends BaseControllerImpl implements BaseController {
 
 	@Reference
 	private RoleService roleService;
 	
+	@Reference
+	private SystemLogService systemLogService;
+	
+	@Reference
+	private RoleUserService roleUserService;
+	
+	@Reference
+	private AdminUserService adminUserService;
+	
+	@Reference
+	private RoleMenuService roleMenuService;
+	
 	@RequestMapping(value = "/list")
 	@Authorize(authorizeResources = false)
 	@ResponseBody
-	public Object list() {
+	public Object list(Boolean isDefault) {
 		ResultData resultData = new ResultData();
 		Pager<Role> pager = new Pager<>();
-		pager.setPageNo(1);
-		pager.setPageSize(Integer.MAX_VALUE);
+//		pager.setPageNo(1);
+//		pager.setPageSize(Integer.MAX_VALUE);
 		pager.getJpqlParameter().setSearchParameter(SearchConstant.IS_TRUE+"_isIsms",true);
+		if(isDefault) {
+			pager.getJpqlParameter().setSearchParameter(SearchConstant.IS_TRUE+"_isDefault",true);
+		}
 		pager =  roleService.findNavigator(pager);
 		resultData.setData("role", pager.getResults());
+		systemLogService.log("role list", this.request.getRequestURL().toString());
 		return resultData;
 	}
 	
@@ -54,24 +86,148 @@ public class RoleController {
 			return resultData;
 		}
 		roleService.save(role);
+		systemLogService.log("add role ", this.request.getRequestURL().toString());
 		return resultData;
 	}
 	
-	@RequestMapping(value = "/roleUser")
+	
+	@RequestMapping(value = "update")
 	@Authorize(authorizeResources = false)
 	@ResponseBody
-	public Object roleUser() {
+	public Object update(@RequestBody Map<String,Object> map) {
 		ResultData resultData = new ResultData();
-		
+		Role role = new Role();
+		MapToBeanUtil.convert(role, map);
+		role.setIsIsms(true);
+		Role oldRole = roleService.findByCode(role.getCode());
+		if(oldRole!=null && !oldRole.getId().equals(oldRole.getId())) {
+			resultData.setMessage(Internationalization.getMessageInternationalization(1400001));
+			resultData.setCode(1400001);
+			return resultData;
+		}
+		roleService.update(role);
+		systemLogService.log("update role ", this.request.getRequestURL().toString());
 		return resultData;
 	}
 	
-	@RequestMapping(value = "/roleMenu")
+	@RequestMapping(value = "/delete")
 	@Authorize(authorizeResources = false)
 	@ResponseBody
-	public Object roleMenu() {
+	public Object delete(@RequestBody String str) throws IOException {
 		ResultData resultData = new ResultData();
+		if (!StringUtils.hasText(str)) {
+			return resultData;
+		}
+		JsonNode jsonNode = new ObjectMapper().readTree(str);
+		if (!jsonNode.has("id")) {
+			return resultData;
+		}
+		ArrayNode arrayNode = (ArrayNode) jsonNode.get("id");
+		for (int i = 0; i < arrayNode.size(); i++) {
+			String id = arrayNode.get(i).asText();
+			try {
+				Role role = this.roleService.findById(id);
+				if(!role.getIsDefault()) {
+					roleService.deleteById(id);
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		systemLogService.log("delete role ", this.request.getRequestURL().toString());
+		return resultData;
+	}
+	
+	@RequestMapping(value = "/user")
+	@Authorize(authorizeResources = false)
+	@ResponseBody
+	public Object roleUser(@RequestBody Map<String,Object> map) {
+		ResultData resultData = new ResultData();
+		RoleUser roleUser = new RoleUser();
+		MapToBeanUtil.convert(roleUser, map);
+		RoleUser oldRoleUser = this.roleUserService.findByRoleIdAndUserId(roleUser.getRoleId(), roleUser.getUserId());
+		if(oldRoleUser!=null) {
+			resultData.setMessage(Internationalization.getMessageInternationalization(1500001));
+			resultData.setCode(1500001);
+			return resultData;
+		}
 		
+		roleUserService.save(roleUser);
+		return resultData;
+	}
+	
+	@RequestMapping(value = "/user/delete")
+	@Authorize(authorizeResources = false)
+	@ResponseBody
+	public Object roleUserDelete(@RequestBody Map<String,Object> map) {
+		ResultData resultData = new ResultData();
+		RoleUser roleUser = this.roleUserService.findByRoleIdAndUserId(map.get("roleId").toString(),map.get("userId").toString());
+		String roleId = roleUser.getRoleId();
+		Role role = this.roleService.findById(roleId);
+		if(!role.getIsDefault()) {
+			this.roleUserService.deleteById(roleUser.getId());
+		}else {
+			resultData.setMessage(Internationalization.getMessageInternationalization(1500002));
+			resultData.setCode(1500002);
+			return resultData;
+		}
+		return resultData;
+	}
+	
+	@RequestMapping(value = "/user/list")
+	@Authorize(authorizeResources = false)
+	@ResponseBody
+	public Object roleUserList(String userId,String roleId) {
+		ResultData resultData = new ResultData();
+		Pager<RoleUser> pager = new Pager<RoleUser>();
+		pager.setPageNo(1);
+		pager.setPageSize(Integer.MAX_VALUE);
+		if(StringUtils.hasText(userId)) {
+			pager.getJpqlParameter().setSearchParameter(SearchConstant.EQUAL+"_userId",userId);
+		}
+		if(StringUtils.hasText(roleId)) {
+			pager.getJpqlParameter().setSearchParameter(SearchConstant.EQUAL+"_roleId",roleId);
+		}
+		pager =  roleUserService.findNavigator(pager);
+		List<RoleUser> list = pager.getResults();
+		List returnList = new  ArrayList();
+		for(RoleUser roleUser : list) {
+			if(StringUtils.hasText(userId)) {
+				Role role = this.roleService.findById(roleUser.getRoleId());
+				returnList.add(role);
+			}
+			if(StringUtils.hasText(roleId)) {
+				AdminUser adminUser = this.adminUserService.findById(roleUser.getUserId());
+				returnList.add(adminUser);
+			}
+		}
+		resultData.setData("list", returnList);
+		systemLogService.log("roleUser list", this.request.getRequestURL().toString());
+		return resultData;
+	}
+	
+	@RequestMapping(value = "/menu")
+	@Authorize(authorizeResources = false)
+	@ResponseBody
+	public Object roleMenu(String roleId,String menuIds) {
+		ResultData resultData = new ResultData();
+		this.roleMenuService.deleteByRoleId(roleId);
+		for(String menuId : menuIds.split(",")) {
+			RoleMenu roleMenu = new RoleMenu();
+			roleMenu.setRoleId(roleId);
+			roleMenu.setMenuId(menuId);
+			roleMenuService.save(roleMenu);
+		}
+		return resultData;
+	}
+	
+	@RequestMapping(value = "/menu/list")
+	@Authorize(authorizeResources = false)
+	@ResponseBody
+	public Object roleMenuList(String roleId) {
+		ResultData resultData = new ResultData();
+		List<String> list = this.roleMenuService.findMenuIdsByRoleId(roleId);
+		resultData.setData("list", list);
 		return resultData;
 	}
 }
